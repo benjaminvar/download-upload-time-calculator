fd3b7e86db1fd522478826bffc1a73c6351c6234
Benjamin Vargas <bvj2206@gmail.com>
2021-03-04 16:15:15 -0400

    Improve header layout

--- src/app/config/en.ts
+++ src/app/config/en.ts
@@ -2,6 +2,7 @@ export default {
     TITLE: "Download/Upload calculator time",
     SIZE: "Download/Upload size",
     SPEED: "Download/Upload speed",
+    UNIT: "Unit",
     CALCULATE: "Calculate",
     ESTIMATED_TIME: "Estimated time",
     LANGUAGE: "Language" 
--- src/app/config/es.ts
+++ src/app/config/es.ts
@@ -2,6 +2,7 @@ export default {
     TITLE: "Calculadora de tiempo de descarga/subida",
     SIZE: "Tamaño de la descarga/subida",
     SPEED: "Velocidad de la descarga/subida",
+    UNIT: "Unidad",
     CALCULATE: "Calcular",
     ESTIMATED_TIME: "Tiempo estimado",
     LANGUAGE: "Idioma"
